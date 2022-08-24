1f5ce005340bb123e183172ccbffc4222775799b
Cadenmf24 <Cadenmf24@gmail.com>
2022-08-23 23:05:16 -0400

    exec_get_one is not defined

--- src/test_chat.py
+++ src/test_chat.py
@@ -4,11 +4,12 @@ from src.swen344_db_utils import connect
 
 class TestChat(unittest.TestCase):
 
-    def test_build_tables(self):
-        """Build the tables"""
-        conn = connect()
-        cur = conn.cursor()
-        buildTables()
-        cur.execute('SELECT * FROM example_table')
-        self.assertEqual([], cur.fetchall(), "no rows in example_table")
-        conn.close()
+    def test_rebuild_tables_is_idempotent(self):
+      """Drop and rebuild the tables twice"""
+      rebuildTables()
+      rebuildTables()
+      conn = connect()
+      cur = conn.cursor()
+      cur.execute('SELECT * FROM example_table')
+      self.assertEqual([], cur.fetchall(), "no rows in example_table")
+      conn.close()
